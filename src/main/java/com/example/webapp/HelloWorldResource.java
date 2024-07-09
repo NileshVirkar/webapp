a61361c951e667062cc97f0eb72bb8c287b408ef
Nilesh <nilesh.virkar@acellere.com>
2024-07-09 10:38:41 +0530

    gitcommit

--- src/main/java/com/example/webapp/HelloWorldResource.java
+++ src/main/java/com/example/webapp/HelloWorldResource.java
@@ -1,10 +1,13 @@
 package com.example.webapp;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +21,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
 import com.google.common.collect.ImmutableList;
 
 @Path("/hello")
@@ -26,18 +33,35 @@ public class HelloWorldResource {
 	private static ExecutorService executor = Executors.newCachedThreadPool();
 
     @GET
+    @Path("/1")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayHello() {
-    	String output = runCmdOld();
+    public String sayHello1() {
+    	String output = runCmdOld("https://acellere.visualstudio.com/Java/_git/Java");
         return output;
     }
     
+    @GET
+    @Path("/2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello2() {
+    	String output = runCmdOld("https://github.com/NileshVirkar/githubdemo");
+        return output;
+    }
     
-    private static String runCmdOld() {
+    @GET
+    @Path("/3")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello3() {
+    	String output = runCmdWithProcessExce();
+        return output;
+    }
+    
+    private static String runCmdOld(String url) {
     	StringBuilder stringBuilder = new StringBuilder();
         System.out.println("--------------------------------------------");
         System.out.println("Current gws command:");
         stringBuilder.append("--------------------------------------------\n");
+        stringBuilder.append("URL : " + url + "\n");
         stringBuilder.append("Current gws command:\n");
         String http_proxy = System.getenv("http_proxy");
         String gws_temp_dir = System.getenv("gws_temp_dir");
@@ -52,7 +76,7 @@ public class HelloWorldResource {
         args2.add("ls-remote");
         args2.add("--heads");
         args2.add("--tags");
-        args2.add("https://acellere.visualstudio.com/Java/_git/Java");
+        args2.add(url);
         try {
             String output = launchCommand2(destinationDir, env, stringBuilder, args2);
             System.out.println("cmd output : " + output);
@@ -128,4 +152,61 @@ public class HelloWorldResource {
         }
         return null;
     }
+    
+	private static String runCmdWithProcessExce() {
+		StringBuilder stringBuilder = new StringBuilder();
+		System.out.println("--------------------------------------------");
+		stringBuilder.append("--------------------------------------------");
+		System.out.println("runCmdWithProcessExce");
+		stringBuilder.append("runCmdWithProcessExce");
+
+		String http_proxy = System.getenv("http_proxy");
+		String gws_temp_dir = System.getenv("gws_temp_dir");
+		stringBuilder.append("http_proxy" + http_proxy);
+		stringBuilder.append("gws_temp_dir" + gws_temp_dir);
+		System.out.println("Current gws command:");
+		stringBuilder.append("Current gws command:");
+		Map<String, String> env = new HashMap<>();
+		if(null != http_proxy) {
+			env.put("http_proxy", http_proxy);
+			env.put("https_proxy", http_proxy);	
+		}
+
+		List<String> arg = new ArrayList<>();
+		arg.add("ls-remote");
+		arg.add("--heads");
+		arg.add("--tags");
+		arg.add("https://acellere.visualstudio.com/Java/_git/Java");
+		String[] arr = new String[arg.size()];
+		String command = "git";
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			executeProcessSync(command, arg.toArray(arr), outputStream, env, stringBuilder);
+			String output = outputStream.toString();
+			System.out.println("output:" + output);
+			stringBuilder.append("output:" + output);
+		} catch (Exception e) {
+			System.err.println("Exception : " + e);
+			stringBuilder.append("Exception : " + e);
+		}
+		return stringBuilder.toString();
+	}
+	
+	public static void executeProcessSync(String executable, String[] args, OutputStream os, Map<String, String> env, StringBuilder stringBuilder){
+		try {
+			stringBuilder.append("Executing synchronous command " + executable + " with args: " + Arrays.toString(args));
+			Map<String, String> envs = new HashMap<String, String>();
+			envs.putAll(env);
+			envs.putAll(System.getenv());
+			CommandLine cmdLine = new CommandLine(executable);
+			cmdLine.addArguments(args, true);
+			PumpStreamHandler streamHandler = new PumpStreamHandler(os);
+			DefaultExecutor executor = new DefaultExecutor();
+			executor.setStreamHandler(streamHandler);
+			executor.execute(cmdLine, envs);
+			stringBuilder.append("*******************");
+		} catch (IOException e) {
+			stringBuilder.append("Exception : " + e);
+		}
+	}
 }
